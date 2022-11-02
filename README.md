5d8215fc1ade132a8ade9374770d1b457b35e27d
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-02 22:20:50 +0530

    added reset feature and made minor changes

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Monthly Expense Tracker
+
+
+# Installation
+First clone this repository in your local directory
+``` bash
+git clone "https://github.com/samyabrata-maji/monthly-expense-tracker" 
+```
+Then create a subfolder named `data`.
+
+Finally open shell and type
+```
+pip install openpyxl
+```
+
+Now run `update.py`
+``` 
+python3 update.py
+```s
--- update.py
+++ update.py
@@ -10,26 +10,29 @@ parent_dir = fr"{os.getcwd()}\data"
 filepath_excel = fr'{parent_dir}\{filename}.xlsx'
 filepath_csv = fr'{parent_dir}\{filename}.csv'
 
-# importing existing data
+# imports csv data in a @param rows
+# eg, csv -> food,40
+#            travel,50
+# then, row[][] = [["food",40], ["travel",50]
 if os.path.exists(filepath_csv):
     with open(filepath_csv, 'r') as csv_file:
         rows = list(csv.reader(csv_file))
 else:
     rows = []
 
-isXPressed = False
+isXPressed = False  # <-- TODO: see if there is a better way to do this
 if input('Do you wish to create entries? (y/n) >>> ') == 'y':
     while not isXPressed:
-        # format
         entry = input('FORMAT: \"tag\",\"expense\" e.g, food,40\nType \'x\' to exit.\n >>> ').strip('\n')
 
         # when 'x' is pressed, loop terminates
+        # TODO: see if there is a better way to do this
         if entry == 'x':
             isXPressed = True
             break
 
         # if input does not match the format,
-        # no exception is thrown
+        # process does not get finished with exit code 0
         try:
             tag = entry.split(',')[0]
             expense = int(entry.split(',')[1])
