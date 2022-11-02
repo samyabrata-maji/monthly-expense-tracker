6ac27553ff473221d6c0139ac9af9b74221d7613
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-02 19:04:05 +0530

    minor changes

--- create.py
+++ create.py
@@ -9,36 +9,3 @@ filename = date.today().strftime('%b_%d_%y')
 parent_dir = fr"{os.getcwd()}\data"
 filepath_excel = fr'{parent_dir}\{filename}.xlsx'
 filepath_csv = fr'{parent_dir}\{filename}.csv'
-
-
-def import_from_csv(worksheet, workbook):
-    # if no csv files exist then creates one
-    # if not os.path.exists(filepath_csv):
-    #   open(filepath_csv, 'x')
-
-    # overwrites data from csv to excel
-    with open(filepath_csv, 'a+') as csv_file:
-        reader = csv.reader(csv_file, delimiter=':')
-        for row in reader:
-            worksheet.append(row)
-    workbook.save(filepath_excel)
-
-
-# if no file with the specific filename exists, then creates one
-# otherwise loads the file
-if not os.path.exists(filepath_excel):
-    wb = openpyxl.Workbook()
-    ws = wb.create_sheet()
-    wb.save(filename=filepath_excel)
-    print(f'File {filename}.xlsx created successfully')
-else:
-    print('File already created')
-    wb = openpyxl.load_workbook(filename=filepath_excel)
-    ws = wb.active
-
-# import data from CSV file to the Excel file of the same name
-if input('Import CSV data? (y/n)') == 'y':
-    print('importing...')
-    import_from_csv(workbook=wb, worksheet=ws)
-
-wb.close()
--- update.py
+++ update.py
@@ -1,3 +1,4 @@
+import csv
 import os
 from csv import writer
 from datetime import date
@@ -9,10 +10,17 @@ parent_dir = fr"{os.getcwd()}\data"
 filepath_excel = fr'{parent_dir}\{filename}.xlsx'
 filepath_csv = fr'{parent_dir}\{filename}.csv'
 
-rows = []
+# importing existing data
+if os.path.exists(filepath_csv):
+    with open(filepath_csv, 'r') as csv_file:
+        rows = list(csv.reader(csv_file))
+else:
+    rows = []
+
 isXPressed = False
 if input('Do you wish to create entries? (y/n) >>> ') == 'y':
     while not isXPressed:
+        # format
         entry = input('FORMAT: \"tag\",\"expense\" e.g, food,40\nType \'x\' to exit.\n >>> ').strip('\n')
 
         # when 'x' is pressed, loop terminates
